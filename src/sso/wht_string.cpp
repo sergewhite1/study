15857dfe66806294a0b20ac3b6e29d91c9a120b1
Sergey Belov <serge_belov@ukr.net>
2020-02-27 20:38:27 +0200

    wht::string. Interface.

--- src/sso/wht_string.cpp
+++ src/sso/wht_string.cpp
@@ -9,8 +9,41 @@ string::string(const char* str) {
   //TODO: wht::string ctor
 }
 
+string::string(const string& obj) {
+	//TODO: wht::string copy ctor
+}
+
+string::string(string&& obj) {
+	//TODO: wht::string move ctor
+}
+
+string& string::operator=(const string& obj) {
+	//TODO: wht::string operator=
+	return *this;
+}
+
+string& string::operator=(string&& obj) {
+	//TODO: wht::string move operator=
+	return *this;
+}
+
 bool string::empty() const {
 	return empty_;
+}
+
+
+size_t string::length() const {
+	//TODO: wht::string::length
+	return 0;
+}
+
+const char* string::data() const {
+	//TODO: wht::string::data
+	return nullptr;
+}
+
+const char* string::c_str() const {
+	//TODO: wht::string::c_str
 }
 
 std::ostream& operator << (std::ostream& rhs, const wht::string& lhs) {
--- src/sso/wht_string.h
+++ src/sso/wht_string.h
@@ -6,7 +6,15 @@ class string {
 public:
 	string();
   explicit string(const char* str);
+  string(const string&);
+  string(string&&);
+  string& operator=(const string&);
+  string& operator=(string&&);
+
   bool empty() const;
+  size_t length() const;
+  const char* data() const;
+  const char* c_str() const;
 
 private:
 	bool empty_ = true;
