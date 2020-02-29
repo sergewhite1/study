0b71b778755ac024a2649be37c913f58e1933d3a
Sergey Belov <serge_belov@ukr.net>
2020-02-29 13:15:05 +0200

    wht::string. Added Unit Tests.

--- test/test-sso/main.cpp
+++ test/test-sso/main.cpp
@@ -1,29 +1,75 @@
+#include <cstring>
 #include <iostream>
 
 #include "wht_string.h"
 
+// See this variable in wht::string class
+static const size_t MAX_LOCAL_SIZE = 32;
+static const char LONG_STRING[]="A VERY LONG STRING BLA_BLA_BLA !!!!! YES!!!";
+
+static_assert(sizeof(LONG_STRING) > MAX_LOCAL_SIZE);
+
 typedef int (*UnitTestProc)();
 
 static int UnitTest0() {
-	// Empty string after constructor
-	// std::cout << "Unit Test0" << std::endl;
-	wht::string s1;
-	if (s1.empty() == false) {
+	// default ctor
+	wht::string s;
+	if (s.empty() == false) {
 		return 1;
 	}
 
-	wht::string s2("");
-	if (s2.empty() == false) {
+	if (s.length() != 0) {
+		return 1;
+	}
+
+	if (strcmp(s.data(), "") != 0) {
+		return 1;
+	} 
+
+	return 0;
+}
+
+static int UnitTest1() {
+	// ctor; short string
+	const char short_string[] = "BLA";
+  const size_t len = strlen(short_string);
+	static_assert(sizeof(short_string) < MAX_LOCAL_SIZE);
+	wht::string s(short_string);
+	if (s.empty() == true) {
+		return 1;
+	}
+	if (s.length() != len) {
+		return 1;
+	}
+	if (strcmp(s.data(), short_string) != 0) {
 		return 1;
 	}
 
 	return 0;
 }
 
-static const UnitTestProc UNIT_TESTS[] = {UnitTest0};
+
+static int UnitTest2() {
+	// ctor; long string
+	const size_t len = strlen(LONG_STRING);
+	wht::string s(LONG_STRING);
+	if (s.empty() == true) {
+		return 1;
+	}
+	if (s.length() != len) {
+		return 1;
+	}
+	if (strcmp(s.data(), LONG_STRING) != 0) {
+		return 1;
+	}
+	return 0;
+}
+
+static const UnitTestProc UNIT_TESTS[] = {UnitTest0, UnitTest1, UnitTest2};
 
 int main(int argc, const char* argv[]) {
 	std::cout << "test-sso"	<< std::endl;
+	std::cout << "sizeof(LONG_STRING)" << sizeof(LONG_STRING) << std::endl;
 
   //std::cout << "argc= " << argc << std::endl;
 
@@ -32,7 +78,6 @@ int main(int argc, const char* argv[]) {
 	//	std::cout << "argv[" << i << "]= " << argv[i] << std::endl;
 	//}
 
-  wht::string s2("");
 	if (argc < 2) {
 		std::cout << "Error! No test case number in command line arg!";
 		return 1;
