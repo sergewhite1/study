2f87622d308a6f919a9d055fffe1bbda23e6e6d3
Sergey Belov <serge_belov@ukr.net>
2020-03-07 05:07:05 +0200

    wht::string. Unit Test. Refactoring.

--- test/test-sso/main.cpp
+++ test/test-sso/main.cpp
@@ -30,798 +30,451 @@ typedef int (*UnitTestProc)();
 
 static int UnitTest01() {
   // default ctor
-  wht::string s;
-  if (s.empty() == false) {
-  	return 1;
-  }
 
-  if (s.length() != 0) {
-  	return 1;
-  }
+  wht::string ws;
 
-  if (strcmp(s.data(), "") != 0) {
-  	return 1;
-  } 
-
-  return 0;
+  return str_test(&ws, true, 0, "");
 }
 
 static int UnitTest02() {
   // ctor; short string
+
   const char short_string[] = "BLA";
   const size_t len = strlen(short_string);
   static_assert(sizeof(short_string) < wht::string::MAX_LOCAL_SIZE, "");
-  wht::string s(short_string);
-  if (s.empty() == true) {
-  	return 1;
-  }
-  if (s.length() != len) {
-  	return 1;
-  }
-  if (strcmp(s.data(), short_string) != 0) {
-  	return 1;
-  }
 
-  return 0;
+  wht::string ws(short_string);
+
+  return str_test(&ws, false, len, short_string);
 }
 
 
 static int UnitTest03() {
   // ctor; long string
+
   const size_t len = strlen(LONG_STRING);
-  wht::string s(LONG_STRING);
-  if (s.empty() == true) {
-  	return 1;
-  }
-  if (s.length() != len) {
-  	return 1;
-  }
-  if (strcmp(s.data(), LONG_STRING) != 0) {
-  	return 1;
-  }
-  return 0;
+
+  wht::string ws(LONG_STRING);
+
+  return str_test(&ws, false, len, LONG_STRING);
 }
 
 
 static int UnitTest04() {
   // ctor; 1 symbol string
+
   const char c[] = "A";
-  wht::string s(c);
-  if (s.empty() == true) {
-  	return 1;
-  }
-  if (s.length() != 1) {
-  	return 1;
-  }
-  if (strcmp(s.data(), c) != 0) {
-  	return 1;
-  }
-  return 0;
+
+  wht::string ws(c);
+
+  return str_test(&ws, false, 1, c);
 }
 
 static int UnitTest05() {
   // ctor; MAX_LOCAL_SIZE-1
-  int ret = 1;
+
   const size_t size = wht::string::MAX_LOCAL_SIZE - 1;
   char *c = create_test_string(size);
 
-  wht::string s(c);
-  do {
-  	if (s.empty() == true) {
-  		break;
-  	}
-  	if (s.length() != size - 1) {
-  		break;
-  	}
-  	if (strcmp(s.data(), c) != 0) {
-  		break;
-  	}
-  	ret = 0;
-  } while(false);
-    
+  wht::string ws(c);
+
+  int ret = str_test(&ws, false, size - 1, c);
   free(c);
+
   return ret;
 }
 
 static int UnitTest06() {
   // ctor; MAX_LOCAL_SIZE
-  int ret = 1;
+
   const size_t size =  wht::string::MAX_LOCAL_SIZE;
   char *c = create_test_string(size);
 
-  wht::string s(c);
-  do {
-  	if (s.empty() == true) {
-  		break;
-  	}
-  	if (s.length() != size - 1) {
-  		break;
-  	}
-  	if (strcmp(s.data(), c) != 0) {
-  		break;
-  	}
-  	ret = 0;
-  } while (false);
+  wht::string ws(c);
 
+  int ret = str_test(&ws, false, size - 1, c);
   free(c);
+
   return ret;
 }
 
 static int UnitTest07() { 
   // ctror; MAX_LOCA_SIZE + 1
-  int ret = 1;
+
   const size_t size =  wht::string::MAX_LOCAL_SIZE + 1;
   char *c = create_test_string(size);
 
-  wht::string s(c);
-  do {
-  	if (s.empty() == true) {
-  		break;
-  	}
-  	if (s.length() != size - 1) {
-  		break;
-  	}
-  	if (strcmp(s.data(), c) != 0) {
-  		break;
-  	}
-  	ret = 0;
-  } while (false);
+  wht::string ws(c);
 
+  int ret = str_test(&ws, false, size - 1, c);
   free(c);
+
   return ret;
 }
 
 
 static int UnitTest08() { 
   // ctor; empty string
-  wht::string s("");
 
-  if (s.empty() == false) {
-  	return 1;
-  }
-  if (s.length() != 0) {
-  	return 1;
-  } 
-  if (*(s.data()) != '\0') {
-  	return 1;
-  }
+  wht::string ws("");
 
-  return 0;
+  return str_test(&ws, true, 0, "");
 }
 
 
 static int UnitTest09() { 
   // ctor; nullptr
-  wht::string s(nullptr);
 
-  if (s.empty() == false)  {
-  	return 1;
-  }
-  if (s.length() != 0) {
-  	return 1;
-  }
-  if (*(s.data()) != '\0') {
-  	return 1;
-  }
+  wht::string ws(nullptr);
 
-  return 0;
+  return str_test(&ws, true, 0, "");
 }
 
 static int UnitTest10() {
   // copy ctor; short string
+
   const char short_string[] = "BLA";
   const size_t size = sizeof(short_string);
   static_assert(size < wht::string::MAX_LOCAL_SIZE, "");
 
-  wht::string s1(short_string);
-  wht::string s2(s1);
-  if (s2.empty() == true) {
-  	return 1;
-  }
-  if (s2.length() != size - 1) {
-  	return 1;
-  }
-  if (strcmp(s2.data(), short_string) != 0){
-  	return 1;
-  }
+  wht::string ws1(short_string);
+  wht::string ws2(ws1);
 
-  return 0;
+  return str_test(&ws2, false, size - 1, short_string);
 } 
 
 static int UnitTest11() {
   // copy ctor; long string
-  wht::string s1(LONG_STRING);
-  wht::string s2(s1);
 
-  if (s2.empty() == true) {
-  	return 1;
-  }
-  if (s2.length() != sizeof(LONG_STRING) - 1) {
-  	return 1;
-  }
-  if (strcmp(s2.data(), LONG_STRING) != 0) {
-  	return 1;
-  }
- 
-  return 0;
+  wht::string ws1(LONG_STRING);
+  wht::string ws2(ws1);
+
+  return str_test(&ws2, false, sizeof(LONG_STRING) - 1, LONG_STRING);
 }
 
 static int UnitTest12() {
   // copy ctor; 1 symbol string
-  const char c[] = "B";
-  wht::string s1(c);
-  wht::string s2(s1);
 
-  if (s2.empty() == true) {
-  	return 1;
-  }
-  if (s2.length() != 1) {
-  	return 1;
-  }
-  if (strcmp(s2.data(), c) != 0) {
-  	return 1;
-  }
- 
-  return 0;
+  const char c[] = "B";
+
+  wht::string ws1(c);
+  wht::string ws2(ws1);
+
+  return str_test(&ws2, false, 1, c);
 }
 
 static int UnitTest13() {
   // copy ctor; MAX_LOCAL_SIZE - 1
-  int ret = 1;
+
   const size_t size = wht::string::MAX_LOCAL_SIZE - 1;
   char *c = create_test_string(size);
 
-  wht::string s1(c);
-  wht::string s2(s1);
-  do {
-  	if (s2.empty() == true) {
-  		break;
-  	}
-  	if (s2.length() != size - 1) {
-  		break;
-  	}
-  	if (strcmp(s2.data(), c) != 0) {
-  		break;
-  	}
-  	ret = 0;
-  } while(false);
-  
+  wht::string ws1(c);
+  wht::string ws2(ws1);
+
+  int ret = str_test(&ws2, false, size - 1, c);
   free(c);
+
   return ret;
 }
 
 static int UnitTest14() {
   // copy ctor; MAX_LOCAL_SIZE
-  int ret = 1;
+
   const size_t size =  wht::string::MAX_LOCAL_SIZE;
   char *c = create_test_string(size);
 
-  wht::string s1(c);
-  wht::string s2(s1);
-  do {
-  	if (s2.empty() == true) {
-  		break;
-  	}
-  	if (s2.length() != size - 1) {
-  		break;
-  	}
-  	if (strcmp(s2.data(), c) != 0) {
-  		break;
-  	}
-  	ret = 0;
-  } while (false);
+  wht::string ws1(c);
+  wht::string ws2(ws1);
 
+  int ret = str_test(&ws2, false, size - 1, c);
   free(c);
+
   return ret;
 }
 
 static int UnitTest15() {
   // copy ctor; MAX_LOCA_SIZE + 1
-  int ret = 1;
+
   const size_t size =  wht::string::MAX_LOCAL_SIZE + 1;
   char *c = create_test_string(size);
 
-  wht::string s1(c);
-  wht::string s2(s1);
-  do {
-  	if (s2.empty() == true) {
-  		break;
-  	}
-  	if (s2.length() != size - 1) {
-  		break;
-  	}
-  	if (strcmp(s2.data(), c) != 0) {
-  		break;
-  	}
-  	ret = 0;
-  } while (false);
+  wht::string ws1(c);
+  wht::string ws2(ws1);
 
+  int ret = str_test(&ws2, false, size - 1, c);
   free(c);
+
   return ret;
 }
 
 static int UnitTest16() {
   // copy ctor; empty string
-  wht::string s1("");
-  wht::string s2(s1);  
 
-  if (s2.empty() == false) {
-  	return 1;
-  }
-  if (s2.length() != 0) {
-  	return 1;
-  } 
-  if (*(s2.data()) != '\0') {
-  	return 1;
-  }
+  wht::string ws1("");
+  wht::string ws2(ws1);  
 
-  wht::string s3;
-  wht::string s4(s3);
+  wht::string ws3;
+  wht::string ws4(ws3);
   
-  if (s4.empty() == false) {
-  	return 1;
-  }
-  if (s4.length() != 0) {
-  	return 1;
-  } 
-  if (*(s4.data()) != '\0') {
-  	return 1;
-  }
-
-  return 0;
+  return str_test(&ws2, true, 0, "") + 
+         str_test(&ws4, true, 0, "");
 }
 
 static int UnitTest17() {
   // copy ctor; nullptr
-  wht::string s1(nullptr);
-  wht::string s2(s1);
 
-  if (s2.empty() == false)  {
-  	return 1;
-  }
-  if (s2.length() != 0) {
-  	return 1;
-  }
-  if (*(s2.data()) != '\0') {
-  	return 1;
-  }
+  wht::string ws1(nullptr);
+  wht::string ws2(ws1);
 
-  return 0;
+  return str_test(&ws2, true, 0, "");
 }
 
 static int UnitTest18() {
   // move ctor; short string
+
   const char short_string[] = "BLA";
   const size_t size = sizeof(short_string);
   static_assert(size < wht::string::MAX_LOCAL_SIZE, "");
 
-  wht::string s1(short_string);
-  wht::string s2(std::move(s1));
-  if (s2.empty() == true) {
-  	return 1;
-  }
-  if (s2.length() != size - 1) {
-  	return 1;
-  }
-  if (strcmp(s2.data(), short_string) != 0){
-  	return 1;
-  }
+  wht::string ws1(short_string);
+  wht::string ws2(std::move(ws1));
 
-  return 0;
+  return str_test(&ws2, false, size - 1, short_string);
 }
 
 static int UnitTest19() {
   // move ctor; long string
-  wht::string s1(LONG_STRING);
-  wht::string s2(std::move(s1));
 
-  if (s2.empty() == true) {
-  	return 1;
-  }
-  if (s2.length() != sizeof(LONG_STRING) - 1) {
-  	return 1;
-  }
-  if (strcmp(s2.data(), LONG_STRING) != 0) {
-  	return 1;
-  }
- 
-  return 0;
+  wht::string ws1(LONG_STRING);
+  wht::string ws2(std::move(ws1));
+
+  return str_test(&ws2, false, sizeof(LONG_STRING) - 1, LONG_STRING);
 }
 
 static int UnitTest20() {
   // move ctor; 1 symbol string
-  const char c[] = "C";
-  wht::string s1(c);
-  wht::string s2(std::move(s1));
 
-  if (s2.empty() == true) {
-  	return 1;
-  }
-  if (s2.length() != 1) {
-  	return 1;
-  }
-  if (strcmp(s2.data(), c) != 0) {
-  	return 1;
-  }
- 
-  return 0;
+  const char c[] = "C";
+  wht::string ws1(c);
+  wht::string ws2(std::move(ws1));
+
+  return str_test(&ws2, false, 1, c);
 }
 
 static int UnitTest21() {
   // move ctor; MAX_LOCAL_SIZE - 1
-  int ret = 1;
+
   const size_t size = wht::string::MAX_LOCAL_SIZE - 1;
   char *c = create_test_string(size);
 
-  wht::string s1(c);
-  wht::string s2(std::move(s1));
-  do {
-  	if (s2.empty() == true) {
-  		break;
-  	}
-  	if (s2.length() != size - 1) {
-  		break;
-  	}
-  	if (strcmp(s2.data(), c) != 0) {
-  		break;
-  	}
-  	ret = 0;
-  } while(false);
-  
+  wht::string ws1(c);
+  wht::string ws2(std::move(ws1));
+
+  int ret = str_test(&ws2, false, size - 1, c);
   free(c);
+
   return ret;
 }
 
 static int UnitTest22() {
   // move ctor; MAX_LOCAL_SIZE
-  int ret = 1;
+
   const size_t size =  wht::string::MAX_LOCAL_SIZE;
   char *c = create_test_string(size);
 
-  wht::string s1(c);
-  wht::string s2(std::move(s1));
-  do {
-  	if (s2.empty() == true) {
-  		break;
-  	}
-  	if (s2.length() != size - 1) {
-  		break;
-  	}
-  	if (strcmp(s2.data(), c) != 0) {
-  		break;
-  	}
-  	ret = 0;
-  } while (false);
+  wht::string ws1(c);
+  wht::string ws2(std::move(ws1));
 
+  int ret = str_test(&ws2, false, size - 1, c);
   free(c);
+
   return ret;
 }
 
 static int UnitTest23() {
   // move ctor; MAX_LOCA_SIZE + 1
-  int ret = 1;
+
   const size_t size =  wht::string::MAX_LOCAL_SIZE + 1;
   char *c = create_test_string(size);
 
-  wht::string s1(c);
-  wht::string s2(std::move(s1));
-  do {
-  	if (s2.empty() == true) {
-  		break;
-  	}
-  	if (s2.length() != size - 1) {
-  		break;
-  	}
-  	if (strcmp(s2.data(), c) != 0) {
-  		break;
-  	}
-  	ret = 0;
-  } while (false);
+  wht::string ws1(c);
+  wht::string ws2(std::move(ws1));
 
+  int ret = str_test(&ws2, false, size - 1, c);
   free(c);
+
   return ret;
 }
 
 static int UnitTest24() {
   // move ctor; empty string
-  wht::string s1("");
-  wht::string s2(std::move(s1));  
 
-  if (s2.empty() == false) {
-  	return 1;
-  }
-  if (s2.length() != 0) {
-  	return 1;
-  } 
-  if (*(s2.data()) != '\0') {
-  	return 1;
-  }
+  wht::string ws1("");
+  wht::string ws2(std::move(ws1));  
 
-  wht::string s3;
-  wht::string s4(std::move(s3));
+  wht::string ws3;
+  wht::string ws4(std::move(ws3));
   
-  if (s4.empty() == false) {
-  	return 1;
-  }
-  if (s4.length() != 0) {
-  	return 1;
-  } 
-  if (*(s4.data()) != '\0') {
-  	return 1;
-  }
-
-  return 0;
+  return str_test(&ws2, true, 0, "") + 
+         str_test(&ws4, true, 0, "");
 }
 
 static int UnitTest25() {
   // move ctor; nullptr
-  wht::string s1(nullptr);
-  wht::string s2(std::move(s1));
 
-  if (s2.empty() == false)  {
-  	return 1;
-  }
-  if (s2.length() != 0) {
-  	return 1;
-  }
-  if (*(s2.data()) != '\0') {
-  	return 1;
-  }
+  wht::string ws1(nullptr);
+  wht::string ws2(std::move(ws1));
 
-  return 0;
+  return str_test(&ws2, true, 0, "");
 }
 
 static int UnitTest26() {
   // operator=; short string
+
   const char short_string[] = "BLA";
   const size_t size = sizeof(short_string);
   static_assert(size < wht::string::MAX_LOCAL_SIZE, "");
 
-  wht::string s1(short_string);
-  wht::string s2("SOME STRING. BLA_BLA_BLA!!!!!!!2");
+  wht::string ws1(short_string);
+  wht::string ws2("SOME STRING. BLA_BLA_BLA!!!!!!!2");
 
-  s2 = s1;
+  ws2 = ws1;
 
-  if (s2.empty() == true) {
-  	return 1;
-  }
-  if (s2.length() != size - 1) {
-  	return 1;
-  }
-  if (strcmp(s2.data(), short_string) != 0){
-  	return 1;
-  }
-
-  return 0;
+  return str_test(&ws2, false, size - 1, short_string);
 }
 
 static int UnitTest27() {
   // operator=; long string
-  wht::string s1(LONG_STRING);
-  wht::string s2("FWEJFKWEJFKWEJWEKJFKLWEJ WDWSDW ssscfscfsdvsdv");
-  
-  s2 = s1;	
 
-  if (s2.empty() == true) {
-  	return 1;
-  }
-  if (s2.length() != sizeof(LONG_STRING) - 1) {
-  	return 1;
-  }
-  if (strcmp(s2.data(), LONG_STRING) != 0) {
-  	return 1;
-  }
- 
-  return 0;
+  wht::string ws1(LONG_STRING);
+  wht::string ws2("FWEJFKWEJFKWEJWEKJFKLWEJ WDWSDW ssscfscfsdvsdv");
+  
+  ws2 = ws1;	
+  return str_test(&ws2, false, sizeof(LONG_STRING) - 1, LONG_STRING);
 }
 
 static int UnitTest28() {
   // operator=; 1 symbol string
+
   const char c[] = "D";
-  wht::string s1(c);
-  wht::string s2("ZXCVVSSDFEEFEFEFJHDFJWHDJWHDJHWJHJHWJDHWJDHJWHDJW");
+  wht::string ws1(c);
+  wht::string ws2("ZXCVVSSDFEEFEFEFJHDFJWHDJWHDJHWJHJHWJDHWJDHJWHDJW");
 
-  s2 = s1;
+  ws2 = ws1;
 
-  if (s2.empty() == true) {
-  	return 1;
-  }
-  if (s2.length() != 1) {
-  	return 1;
-  }
-  if (strcmp(s2.data(), c) != 0) {
-  	return 1;
-  }
- 
-  return 0;
+  return str_test(&ws2, false, 1, c);
 }
 
 static int UnitTest29() {
   // operator=; MAX_LOCAL_SIZE - 1
-  int ret = 1;
+
   const size_t size = wht::string::MAX_LOCAL_SIZE - 1;
   char *c = create_test_string(size);
 
-  wht::string s1(c);
-  wht::string s2("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH");
+  wht::string ws1(c);
+  wht::string ws2("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH");
 
-  s2 = s1;
+  ws2 = ws1;
 
-  do {
-  	if (s2.empty() == true) {
-  		break;
-  	}
-  	if (s2.length() != size - 1) {
-  		break;
-  	}
-  	if (strcmp(s2.data(), c) != 0) {
-  		break;
-  	}
-  	ret = 0;
-  } while(false);
-  
+  int ret = str_test(&ws2, false, size - 1, c); 
   free(c);
+
   return ret;
 }
 
 static int UnitTest30() {
   // operator=; MAX_LOCAL_SIZE
-  int ret = 1;
+
   const size_t size =  wht::string::MAX_LOCAL_SIZE;
   char *c = create_test_string(size);
 
-  wht::string s1(c);
-  wht::string s2("hello");
+  wht::string ws1(c);
+  wht::string ws2("hello");
 
-  s2 = s1;
-  
-  do {
-  	if (s2.empty() == true) {
-  		break;
-  	}
-  	if (s2.length() != size - 1) {
-  		break;
-  	}
-  	if (strcmp(s2.data(), c) != 0) {
-  		break;
-  	}
-  	ret = 0;
-  } while (false);
+  ws2 = ws1;
 
+  int ret = str_test(&ws2, false, size - 1, c);
   free(c);
+
   return ret;
 }
 
 static int UnitTest31() {
   // operator=; MAX_LOCA_SIZE + 1
-  int ret = 1;
+
   const size_t size = wht::string::MAX_LOCAL_SIZE + 1;
   char *c = create_test_string(size);
 
-  wht::string s1(c);
-  wht::string s2("IIIIIIIIIOOOOOOOOOOPPPPPPPPPPPPZZZZZZZZZZZXXXXXXXXXXXCCV");
+  wht::string ws1(c);
+  wht::string ws2("IIIIIIIIIOOOOOOOOOOPPPPPPPPPPPPZZZZZZZZZZZXXXXXXXXXXXCCV");
 
-  s2 = s1;
+  ws2 = ws1;
 
-  do {
-  	if (s2.empty() == true) {
-  		break;
-  	}
-  	if (s2.length() != size - 1) {
-  		break;
-  	}
-  	if (strcmp(s2.data(), c) != 0) {
-  		break;
-  	}
-  	ret = 0;
-  } while (false);
-
+  int ret = str_test(&ws2, false, size - 1, c);
   free(c);
+
   return ret;
 }
 
 static int UnitTest32() {
   // operator=;  empty string
-  wht::string s1("");
-  wht::string s2("hello");  
 
-  s2 = s1;
+  wht::string ws1("");
+  wht::string ws2("hello");  
 
-  if (s2.empty() == false) {
-  	return 1;
-  }
-  if (s2.length() != 0) {
-  	return 1;
-  } 
-  if (*(s2.data()) != '\0') {
-  	return 1;
-  }
+  ws2 = ws1;
 
-  wht::string s3;
-  wht::string s4("DHJFHDJHFJDHFJDHFJKHJFKLAKL:DKLASDKLSDKLSKLDKLSKDLSKDLSKDL");
+  wht::string ws3;
+  wht::string ws4("DHJFHDJHFJDHFJDHFJKHJFKLAKL:DKLASDKLSDKLSKLDKLSKDLSKDLSKDL");
 
-  s4 = s3;  
+  ws4 = ws3;  
 
-  if (s4.empty() == false) {
-  	return 1;
-  }
-  if (s4.length() != 0) {
-  	return 1;
-  } 
-  if (*(s4.data()) != '\0') {
-  	return 1;
-  }
-
-  return 0;
+  return str_test(&ws2, true, 0, "") + 
+         str_test(&ws4, true, 0, "");
 }
 
 static int UnitTest33() {
   // operator=; nullptr
-  wht::string s1(nullptr);
-  wht::string s2("abcd !!!!!d");
 
-  s2 = s1;
+  wht::string ws1(nullptr);
+  wht::string ws2("abcd !!!!!d");
 
-  if (s2.empty() == false)  {
-  	return 1;
-  }
-  if (s2.length() != 0) {
-  	return 1;
-  }
-  if (*(s2.data()) != '\0') {
-  	return 1;
-  }
+  ws2 = ws1;
 
-  return 0;
+  return str_test(&ws2, true, 0, "");
 }
 
 static int UnitTest34() {
   // operator=; self assign
+
   const char str[]="hello";
   const size_t size = sizeof(str);
-  wht::string s1(str);
-  wht::string &s2 = s1;
+
+  wht::string ws1(str);
+  wht::string &ws2 = ws1;
   
-  s2 = s1;
+  ws2 = ws1;
 
-  if (s1.empty() == true) {
-  	return 1;
-  }
-  if (s1.length() != size - 1) {
-  	return 1;
-  }
-  if (strcmp(s1.data(), str) != 0) {
-  	return 1;
-  }
-
-  if (s2.empty() == true) {
-  	return 1;
-  }
-  if (s2.length() != size - 1) {
-  	return 1;
-  }
-  if (strcmp(s2.data(), str) != 0) {
-  	return 1;
-  }
-
-  return 0;
+  return str_test(&ws1, false, size - 1, str);
 }
 
 static int UnitTest35() {
   // move operator=; (short string) = (short string)
+
   const char str1[] = "hello";
   static_assert(sizeof(str1) < wht::string::MAX_LOCAL_SIZE, "");
 
   const char str2[] = "short";
   static_assert(sizeof(str2) < wht::string::MAX_LOCAL_SIZE, "");
 
-  wht::string s1(str1);
-  wht::string s2(str2);
+  wht::string ws1(str1);
+  wht::string ws2(str2);
   
-  s1 = std::move(s2);
+  ws1 = std::move(ws2);
 
-  return str_test(&s1, false, sizeof(str2) - 1, str2) + 
-         str_test(&s2, false, sizeof(str1) - 1, str1);
+  return str_test(&ws1, false, sizeof(str2) - 1, str2) + 
+         str_test(&ws2, false, sizeof(str1) - 1, str1);
 }
 
 static const UnitTestProc UNIT_TESTS[] = {nullptr, 
