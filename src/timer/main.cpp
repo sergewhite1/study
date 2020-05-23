b7628c8ab6653a6ba8b0b19eabdafa97e8e1a7de
Sergey Belov <serge_belov@ukr.net>
2020-05-23 13:26:36 +0300

    Timer. Do not do optimization using volatile.

--- src/timer/main.cpp
+++ src/timer/main.cpp
@@ -6,7 +6,7 @@
 int main() {
   std::cout << "timer demo" << std::endl;
   {
-    technosphera::Timer t;
+    volatile technosphera::Timer t;
     std::this_thread::sleep_for(std::chrono::seconds(1));
   }
   return 0;
