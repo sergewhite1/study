1c81f24f960530a38725aa9da05536e790c61946
Sergey Belov <serge_belov@ukr.net>
2025-02-21 13:35:30 +0200

    In progress. Added validation function.

--- latex/latexhelp/latexhelp.py
+++ latex/latexhelp/latexhelp.py
@@ -28,7 +28,7 @@ class LatexHelp:
         with open(filename, 'r') as f:
             self._data = json.load(f)
 
-        print(f"LatexHelp.load. self._data={self._data}")
+        # print(f"LatexHelp.load. self._data={self._data}")
 
         ret = self._validate()
 
@@ -51,7 +51,75 @@ class LatexHelp:
         print(f"File {filename} has been created successfully.")
 
     def _validate(self) -> bool:
-        print("TODO: Inside validate")
+        if "latexhelp" not in self._data:
+            print("LatexHelp validate error. Array \"latexhelp\" is absent!")
+            return False
+
+        index = 0
+
+        # This variables are used to check the uniqueness of an entity
+        command_set = set()
+        env_set = set()
+
+        for entity in self._data["latexhelp"]:
+            if not self._validate_entity(entity, index, command_set, env_set):
+                return False
+            index += 1
+
+        return True
+
+    def _validate_entity(self,
+                         entity: dict,
+                         index: int,
+                         command_set: set,
+                         env_set: set) -> bool:
+        if "entity" not in entity:
+            print(f"LatexHelp validate error. Index: {index}. "
+                  f"Parameter \"entity\" not found in entity: {entity}")
+            return False
+
+        if "type" not in entity:
+            print(f"LatexHelp validate error. Index: {index}. "
+                  f"Parameter \"type\" not found in "
+                  f"entity: {entity['entity']}")
+            return False
+
+        if entity["type"] == "command":
+            if entity["entity"] in command_set:
+                print(f"LatexHelp validate error. Index: {index}. "
+                      f"Command {entity['entity']} has been already added.")
+                return False
+            command_set.add(entity["entity"])
+
+        elif entity["type"] == "environment":
+            if entity["entity"] in env_set:
+                print(f"LatexHelp validate error. Index: {index}. "
+                      f"Environment {entity['entity']} "
+                      f"has been already added.")
+                return False
+            env_set.add(entity["entity"])
+
+        else:
+            print(f"LatexHelp validate error. Index: {index}. "
+                  f"Entity: {entity['entity']}. "
+                  f"Unexpected \"type\" value \"{entity['type']}\". "
+                  f"Expected values: command, environment.")
+            return False
+
+        if "package" not in entity:
+            print(f"LatexHelp validate error. Index: {index}. "
+                  f"Entity {entity['entity']}. "
+                  f"Parameter \"package\" is absent.")
+            return False
+
+        if "description" not in entity:
+            print(f"LatexHelp validate error. Index: {index}. "
+                  f"Entity {entity['entity']}. "
+                  f"Parameter \"description\" is absent.")
+            return False
+
+        # TODO: Arguments check
+
         return True
 
     def _write_entity(self, f, entity):
